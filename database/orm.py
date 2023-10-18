ac7371b963e678a3880f69b580414d0d8f9e4dc4
YakutinSergei <yakutinserg@yandex.ru>
2023-10-18 18:15:21 +0300

    пар

--- database/orm.py
+++ database/orm.py
@@ -326,8 +326,8 @@ async def get_opp_commands(tg_id):
                                             THEN 0 WHEN team.forward_2 = player_id THEN 1 
                                             WHEN team.forward_3 = player_id THEN 2 
                                             WHEN team.defender_1 = player_id THEN 3 
-                                            WHEN team.defender_2 = player_id THEN 4 END
-                                            ORDER BY random() LIMIT 1
+                                            WHEN team.defender_2 = player_id THEN 4 END,
+                                            random() LIMIT 1
                 ''')
         return teams
 
